223e480be75e96a7e8b3b0f50bdd2eab928f2795
Kartik Ayyer <kartik.ayyer@desy.de>
2025-06-25 21:29:29 +0900

    constants: Added photon conversion information

--- constants.py
+++ constants.py
@@ -2,11 +2,20 @@ PREFIX = '/work/kayyer/2025A8054/'
 DET_NAME = 'MPCCD-2N0-M02-001'
 DET_SHAPE = (2, 1024, 512)
 BL_NUM = 3
+
+# Auto-logging
 RUN_INIT = 1561457
 COLUMNS = ['starttime', 'stoptime', 'total_tagnumber',
            'start_tagnumber', 'end_tagnumber', 'hightagnumber',
            'comment', 'runtype', 'stationnumber', 'runstatus']
-BATCH_SIZE = 100
-ADU_THRESHOLD = 10
+
+# Hit finding
+ADU_THRESHOLD = 50
+
+# Assembly
 ASSEM_SHAPE = (1100, 1200)
 ASSEM_SHIFTS = ([38, 38], [39, 571])
+
+# Photon conversion
+MOD_GAINS = (25.2525, 25.8232)
+ADU_PER_PHOTON = 96.5
