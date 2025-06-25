253a5bc7f9455a2d49ecf7158daf8886d1a85770
Kartik Ayyer <kartik.ayyer@desy.de>
2025-06-25 11:33:17 +0900

    constants: Added logger constants and ADU_THRESHOLD for litpixels

--- constants.py
+++ constants.py
@@ -1,5 +1,10 @@
 PREFIX = '/work/kayyer/2025A8054/'
 DET_NAME = 'MPCCD-2N0-M02-001'
 BL_NUM = 3
+RUN_INIT = 1561457
+COLUMNS = ['starttime', 'stoptime', 'total_tagnumber',
+           'start_tagnumber', 'end_tagnumber', 'hightagnumber',
+           'comment', 'runtype', 'stationnumber', 'runstatus']
 BATCH_SIZE = 100
 DET_SHAPE = (1100, 1200)
+ADU_THRESHOLD = 10
