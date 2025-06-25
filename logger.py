1a7174785ddfde2248def6699287e7a36860d947
Kartik Ayyer <kartik.ayyer@desy.de>
2025-06-25 21:29:49 +0900

    logger: Fixed logic waiting for new runs

--- logger.py
+++ logger.py
@@ -17,15 +17,21 @@ except FileNotFoundError:
     runs_df = pd.DataFrame(columns=COLUMNS)
 
 # Wait for new runs to arrive
+#latest = dbpy.read_runnumber_newest(BL_NUM)
 while True:
-    latest = dbpy.read_runnumber_newest(BL_NUM)
-    if latest < run:
+    try:
+        rinfo = dbpy.read_runinfo(BL_NUM, run)
+    except dbpy.APIError:
+        print('Waiting for run %d' % run)
         time.sleep(5)
         continue
-    rinfo = dbpy.read_runinfo(BL_NUM, run)
+    if rinfo['runstatus'] != 0:
+        print('%d running'%run)
+        time.sleep(5)
+        continue
     runs_df.loc[run] = [rinfo[k] for k in COLUMNS]
     runs_df.to_csv(runs_fname)
     os.chmod(runs_fname, 0o664)
-    print(run)
+    print('%d logged'%run)
     #print(run, rinfo.keys(), rinfo['start_tagnumber'], rinfo['end_tagnumber'])
     run += 1
