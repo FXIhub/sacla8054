2b61279c5b38f31c3b7a4beccca0c0d874ef1bd1
Kartik Ayyer <kartik.ayyer@desy.de>
2025-06-25 14:50:06 +0900

    utils: Added assemble function

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import dbpy, stpy
 import numpy as np
 
 from constants import BL_NUM, DET_NAME
+from constants import DET_SHAPE, ASSEM_SHAPE, ASSEM_SHIFTS
 
 def get_tags(run):
     return dbpy.read_taglist_byrun(BL_NUM, run)
@@ -19,3 +20,10 @@ def get_frames(run, indices, taglist=None):
         frames.append(buff.read_det_data(0))
     return frames
 
+def assemble(frame):
+    assert frame.shape == DET_SHAPE
+    assem = np.zeros(ASSEM_SHAPE, dtype='f4')
+    s1, s2 = ASSEM_SHIFTS
+    assem[s1[0]:s1[0]+DET_SHAPE[1], s1[1]:s1[1]+DET_SHAPE[2]] = frame[0]
+    assem[s2[0]:s2[0]+DET_SHAPE[1], s2[1]:s2[1]+DET_SHAPE[2]] = frame[1]
+    return assem
