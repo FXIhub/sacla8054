df812ccba979c73bfc7f07acce3471bcc6eb33f4
Kartik Ayyer <kartik.ayyer@desy.de>
2025-06-25 14:40:16 +0900

    constants: Added assembly constants

--- constants.py
+++ constants.py
@@ -1,10 +1,12 @@
 PREFIX = '/work/kayyer/2025A8054/'
 DET_NAME = 'MPCCD-2N0-M02-001'
+DET_SHAPE = (2, 1024, 512)
 BL_NUM = 3
 RUN_INIT = 1561457
 COLUMNS = ['starttime', 'stoptime', 'total_tagnumber',
            'start_tagnumber', 'end_tagnumber', 'hightagnumber',
            'comment', 'runtype', 'stationnumber', 'runstatus']
 BATCH_SIZE = 100
-DET_SHAPE = (1100, 1200)
 ADU_THRESHOLD = 10
+ASSEM_SHAPE = (1100, 1200)
+ASSEM_SHIFTS = ([38, 38], [39, 571])
