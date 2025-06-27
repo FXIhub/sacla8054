32ecff36f1c6583cb180be2fef0a247de0695cb8
Kartik Ayyer <kartik.ayyer@desy.de>
2025-06-27 09:29:00 +0900

    litpixels_array: Updated mask

--- pbs/litpixels_array.sh
+++ pbs/litpixels_array.sh
@@ -11,7 +11,8 @@ cd ${PBS_O_WORKDIR}
 module load python
 PREFIX=/home/kayyer/2025A8054/ana
 
-run=1561815
+run=1561885
 #python ${PREFIX}/litpixels.py $run -m ${PREFIX}/data/geom/goodpix_highq.npy
-python ${PREFIX}/litpixels.py $run -m ${PREFIX}/data/geom/goodpix_highq_v2.npy
+#python ${PREFIX}/litpixels.py $run -m ${PREFIX}/data/geom/goodpix_highq_v2.npy
+python ${PREFIX}/litpixels.py $run -m ${PREFIX}/data/geom/goodpix_highq_v3.npy
 python ${PREFIX}/add_motors.py $run
